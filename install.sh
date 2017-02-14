5b6717f4cee23ad6cae5a9568c1d66e8fa5e372a
Mirza Karacic <micomirza@gmail.com>
2017-02-14 15:56:27 -0600

    Additional files

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install ohmyzsh
+sudo pacman -S zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install antigen
+curl https://cdn.rawgit.com/zsh-users/antigen/v1.4.0/bin/antigen.zsh > ~/.antigen.zsh
